d0a3ee2f7412d0dc1c280ae2c17c2570882e58ae
Arpi <arpishah2@gmail.com>
2018-07-07 17:56:46 -0700

    added links to producList and home view

---
+++ java/com/emusicstore/dao/ProductDao.java
@@ -0,0 +1,57 @@
+package com.emusicstore.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.emusicstore.model.Product;
+
+public class ProductDao {
+	private List<Product> productList;
+
+	public List<Product> getProductList() {
+		Product product1 = new Product();
+		product1.setProductName("Guitar");
+		product1.setProductCategory("Instrument");
+		product1.setProductDescription("This is an amazing Guitar");
+		product1.setProductPrice(1200);
+		product1.setProductCondition("new");
+		product1.setProductStatus("active");
+		product1.setUnitInStock(11);
+		product1.setProductManufacturer("Fender");
+		
+		
+		Product product2 = new Product();
+		product2.setProductName("Record1");
+		product2.setProductCategory("Record");
+		product2.setProductDescription("This is an amazing mix of 20th century");
+		product2.setProductPrice(25);
+		product2.setProductCondition("new");
+		product2.setProductStatus("active");
+		product2.setUnitInStock(51);
+		product2.setProductManufacturer("EMI");
+		
+		Product product3 = new Product();
+		product3.setProductName("Speaker1");
+		product3.setProductCategory("Accessory");
+		product3.setProductDescription("This is a polk shelf speaker");
+		product3.setProductPrice(355);
+		product3.setProductCondition("new");
+		product3.setProductStatus("active");
+		product3.setUnitInStock(9);
+		product3.setProductManufacturer("Polk");
+		
+		productList = new ArrayList<Product>();
+		productList.add(product1);
+		productList.add(product2);
+		productList.add(product3);
+		
+		return productList;
+	}
+
+	public void setProductList(List<Product> productList) {
+		this.productList = productList;
+	}
+	
+	
+	
+}
